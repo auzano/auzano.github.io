313c622c565736b8f2daa6e35ac88b6a75dcad14
auzano <auzano@investree.id>
2024-12-26 23:48:26 +0700

    brand new

--- js/script.js
+++ js/script.js
@@ -1,62 +1,19 @@
-// event pada saat link di klik
-$('.page-scroll').on('click', function(e){
+// Select all elements with the class "hidden"
+const hiddenElements = document.querySelectorAll('.hidden');
 
-	// ambil isi href
-	var tujuan = $(this).attr('href');
-	// tangkap elemen ybs
-	var elemenTujuan = $(tujuan);
-
-	// pindahkan scroll
-	$('body').animate({
-		scrollTop: elemenTujuan.offset().top - 50
-	}, 1250, 'easeInOutExpo');
-
-	e.preventDefault();
-
+// Create an Intersection Observer
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible'); // Add "visible" class when in viewport
+            observer.unobserve(entry.target); // Stop observing once visible
+        }
+    });
+}, {
+    threshold: 0.5 // Trigger when 20% of the element is visible
 });
 
-// parallax
-$(window).on('load', function(){
-	$('.kiri').addClass('kiri-go');
-	$('.kanan').addClass('kanan-go')
+// Observe each hidden element
+hiddenElements.forEach(element => {
+    observer.observe(element);
 });
-
-$(window).scroll(function(){
-	var wScroll = $(this).scrollTop();
-
-	$('.jumbotron img').css({
-		'transform': 'translate(0px, '+ wScroll / 4 +'%)'
-	});
-
-	$('.jumbotron h1').css({
-		'transform': 'translate(0px, '+ wScroll / 2 +'%)'
-	});
-
-	$('.jumbotron p').css({
-		'transform': 'translate(0px, '+ wScroll / 1.2 +'%)'
-	});
-
-
-	// portfolio
-	if( wScroll > $('.portfolio').offset().top - 600 ) {
-		
-		$('.portfolio .thumbnail').each(function(i) {
-			setTimeout(function() {
-				$('.portfolio .thumbnail').eq(i).addClass('muncul');
-			}, 180 * (i+1))
-
-		});
-
-	}
-
-});
-
-
-
-
-
-
-
-
-
-
